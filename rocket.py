1660e82195ab18b094a45e530988b2047c5e534b
Andreina <andreina.c.tirado@gmail.com>
2019-08-24 21:10:13 +0200

    initial commit for Alien Invasion game

---
+++ rocket.py
@@ -0,0 +1,21 @@
+import pygame
+
+class Rocket:
+    """Rocket representation"""
+    def __init__(self, ai_game):
+        # set the initial position
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+
+
+        # load the image and get the rect
+        self.image = pygame.image.load('img/rocket.bmp')
+        self.rect = self.image.get_rect()
+           
+        # Set the position
+        self.rect.midbottom = self.screen_rect.midbottom
+
+    def blitme(self):
+        """Draw the ship at its current location."""
+        self.screen.blit(self.image, self.rect)
+
---
+++ settings.py
@@ -0,0 +1,8 @@
+class Settings:
+    """ This class will store all settings related to the game. """
+
+    def __init__(self):
+        self.width = 1000
+        self.height = 600
+        self.color = (255, 255, 255)
+        self.title = "Alien Invasion"
