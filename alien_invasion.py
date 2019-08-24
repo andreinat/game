92b5799c06c5c596ac8aedc075016b33408a2415
Andreina <andreina.c.tirado@gmail.com>
2019-08-24 23:20:40 +0200

    create methods to handle events and the screen update

--- alien_invasion.py
+++ alien_invasion.py
@@ -9,25 +9,36 @@ class AlienInvasion:
     def __init__(self):
         """Initialize the game, and create game resources."""
         pygame.init()
-
+        # General settings
         self.settings = Settings()
+        # Screen
         self.screen = pygame.display.set_mode((self.settings.width, self.settings.height))
         pygame.display.set_caption(self.settings.title)
+        # Rocket
         self.rocket = Rocket(self)
 
     def run_game(self):
         """Start the main loop for the game."""
         while True:
-            # Watch for keyboard and mouse events.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
- 
-            # Redraw the screen during each pass through the loop.
-            self.screen.fill(self.settings.color)
-            self.rocket.blitme()
-            # Make the most recently drawn screen visible.
-            pygame.display.flip()
+            # Get events
+            self.get_events()
+            self.update_screen()
+
+            
+    
+    def get_events(self):
+        # Watch for keyboard and mouse events.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+    
+    def update_screen(self):
+        # Redraw the screen during each pass through the loop.
+        self.screen.fill(self.settings.color)
+        self.rocket.blitme()
+        # Make the most recently drawn screen visible.
+        pygame.display.flip()
+
 
 if __name__ == '__main__':
     # Make a game instance, and run the game.
